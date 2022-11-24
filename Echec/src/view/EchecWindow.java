a54eb1a2a8bc777a84ea477f0f19046c4f622dbf
Arnaud NOEL <arnaud.noel@unistra.fr>
2022-11-24 17:22:29 +0100

    Modification de diagramme de classe

--- Echec/src/view/EchecWindow.java
+++ Echec/src/view/EchecWindow.java
@@ -1,7 +1,19 @@
 package view;
 
+import Controller.Facade;
+
+import javax.swing.*;
+import java.awt.*;
 import javax.swing.*;
 import java.awt.*;
 
-public class EchecWindow {
+public class EchecWindow extends JFrame{
+    private JButton[][] Echecquier;
+
+    public static void EchecWindow()
+    {
+        JButton temp;
+
+
+    }
 }
