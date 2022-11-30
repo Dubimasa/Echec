bf4aa94b99c9ba710d238b1e380bba6dab310af4
Arnaud NOEL <arnaud.noel@unistra.fr>
2022-11-30 09:29:47 +0100

    Ajout des modification du puml

--- Echec/src/view/EchecWindow.java
+++ Echec/src/view/EchecWindow.java
@@ -8,12 +8,30 @@ import javax.swing.*;
 import java.awt.*;
 
 public class EchecWindow extends JFrame{
-    private JButton[][] Echecquier;
+    private static JButton[][] Echecquier = new JButton[8][8];
+    private JPanel Affiche;
 
-    public static void EchecWindow()
+    public EchecWindow()
     {
+        setTitle("Echec");
+        setSize(600, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Affiche = new JPanel();
         JButton temp;
+        for(int x = 0; x<8; x++)
+        {
+            for(int y =0; y<8;y++)
+            {
+                temp = new JButton();
+                int finalX = x;
+                int finalY = y;
 
+                temp.addActionListener(actionEvent -> {
+                    System.out.println("X = " + finalX +" Y = " + finalY);
+                });
+                Echecquier[x][y] = temp;
+            }
+        }
 
     }
 }
